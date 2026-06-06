9713df2ed1d4a04966bbecf8bf72a672d4565e1f
Dicky Sanjaya <dickysanjayaputra2101@gmail.com>
2026-06-06 19:16:59 +0700

    agar url ttp https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Memaksa semua URL menggunakan HTTPS jika berjalan di Railway/Production
+        if (config('app.env') === 'production' || isset($_SERVER['HTTPS'])) {
+            URL::forceScheme('https');
+        }
     }
 }
